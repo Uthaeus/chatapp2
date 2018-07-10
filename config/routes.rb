d86454bd17a69dae4f9d0fc3561b6f21201e3830
Uthaeus <romanlavery@gmail.com>
2018-07-09 20:01:43 -0600

    Create new templates for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :destroy, :create]
+  
 end
